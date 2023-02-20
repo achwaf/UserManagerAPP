279c34ed861d0a54a0550d601ffa92187c7123be
achwaf <achwaf@gmail.com>
2023-02-20 03:00:02 +0100

    some adjustment in the character

--- src/app/model/avatar-character-enum.ts
+++ src/app/model/avatar-character-enum.ts
@@ -2,5 +2,5 @@ export enum AvatarCharacter
  {
     NOT_REACTIVE = 0, // does react at all
     LESS_REACTIVE = 3, // 1/3 chance to ract
-    MORE_REACTIVE = 1.5, // 2/3 chance to react
+    MORE_REACTIVE = 2, // 1/2 chance to react
 }
